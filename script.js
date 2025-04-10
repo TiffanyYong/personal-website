4fda33fbb2003ed6d1d9617d016124b662863246
Tiffany Yong <tiffanyyong28@gmail.com>
2025-04-10 14:25:18 -0400

    remove cached favicon

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const getFaviconFormat = () => {
     const link = document.createElement('link');
     const supportsSvg = link.relList && link.relList.supports && link.relList.supports('icon') && !!document.createElementNS;
     if (supportsSvg) return 'svg';
-    
+
     const canvas = document.createElement('canvas');
     return !!(canvas.getContext && canvas.getContext('2d')) ? 'png' : 'ico';
   };
@@ -10,6 +10,7 @@ const getFaviconFormat = () => {
   const setFavicon = (scheme) => {
     const favicon = document.getElementById('dynamic-favicon');
     const format = getFaviconFormat();
+    const timestamp = new Date().getTime(); // Cache busting
 
     let filename = '';
     let type = '';
@@ -25,12 +26,16 @@ const getFaviconFormat = () => {
       type = 'image/x-icon';
     }
 
-    favicon.href = `/favicons/${filename}`;
+    favicon.href = `/favicons/${filename}?t=${timestamp}`;  // Cache busting
     favicon.type = type;
   };
 
+  window.onload = () => {
+    const match = window.matchMedia('(prefers-color-scheme: dark)');
+    setFavicon(match.matches ? 'dark' : 'light');
+  };
+
   const match = window.matchMedia('(prefers-color-scheme: dark)');
-  setFavicon(match.matches ? 'dark' : 'light');
   match.addEventListener('change', (e) => {
     setFavicon(e.matches ? 'dark' : 'light');
   });
