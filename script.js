a31fe51c40292e0815e5e859a88ff28c3380c062
Tiffany Yong <tiffanyyong28@gmail.com>
2025-04-10 14:29:31 -0400

    change fallback favicon

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const getFaviconFormat = () => {
     const link = document.createElement('link');
     const supportsSvg = link.relList && link.relList.supports && link.relList.supports('icon') && !!document.createElementNS;
     if (supportsSvg) return 'svg';
-
+    
     const canvas = document.createElement('canvas');
     return !!(canvas.getContext && canvas.getContext('2d')) ? 'png' : 'ico';
   };
@@ -10,7 +10,6 @@ const getFaviconFormat = () => {
   const setFavicon = (scheme) => {
     const favicon = document.getElementById('dynamic-favicon');
     const format = getFaviconFormat();
-    const timestamp = new Date().getTime(); // Cache busting
 
     let filename = '';
     let type = '';
@@ -26,16 +25,12 @@ const getFaviconFormat = () => {
       type = 'image/x-icon';
     }
 
-    favicon.href = `/favicons/${filename}?t=${timestamp}`;  // Cache busting
+    favicon.href = `/favicons/${filename}`;
     favicon.type = type;
   };
 
-  window.onload = () => {
-    const match = window.matchMedia('(prefers-color-scheme: dark)');
-    setFavicon(match.matches ? 'dark' : 'light');
-  };
-
   const match = window.matchMedia('(prefers-color-scheme: dark)');
+  setFavicon(match.matches ? 'dark' : 'light');
   match.addEventListener('change', (e) => {
     setFavicon(e.matches ? 'dark' : 'light');
   });
